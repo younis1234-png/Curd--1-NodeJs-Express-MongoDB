c90b0ebd2127c865f69998829f218d23c86b38a1
Younis <younis@Youniss-MacBook-Pro.local>
2021-05-24 19:40:09 -0400

    add user id and user post request to add aor create a new user

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,18 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const { v4: uuidv4 } = require("uuid");
 
 const users = [
   {
     name: "Browm",
     lasName: "Tom",
-    age: "25",
+    age: "23",
+  },
+  {
+    name: "Summi",
+    lasName: "Ali",
+    age: "19",
   },
 ];
 
 // all routes in here start with /users
 router.get("/", (req, res) => {
-  console.log(users);
   res.send(users);
+});
+
+router.post("/", (req, res) => {
+  // push our new data to our array
+  const user = req.body;
+  const userId = uuidv4();
+  // and add the unique id
+  const userWithId = { ...user, id: userId };
+  users.push(userWithId);
+  res.send(`Users with the name ${user.name} added to the database!`);
 });
 
 module.exports = router;
