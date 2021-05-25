44c3b59fb7dede5373ebedc59ee5f3a76988a5ca
Younis <younis@Youniss-MacBook-Pro.local>
2021-05-24 20:32:46 -0400

    last commit

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,17 +2,17 @@ const express = require("express");
 const router = express.Router();
 const { v4: uuidv4 } = require("uuid");
 
-const users = [
-  {
-    name: "Browm",
-    lasName: "Tom",
-    age: "23",
-  },
-  {
-    name: "Summi",
-    lasName: "Ali",
-    age: "19",
-  },
+let users = [
+  //   {
+  //     name: "Browm",
+  //     lasName: "Tom",
+  //     age: "23",
+  //   },
+  //   {
+  //     name: "Summi",
+  //     lasName: "Ali",
+  //     age: "19",
+  //   },
 ];
 
 // all routes in here start with /users
@@ -30,4 +30,40 @@ router.post("/", (req, res) => {
   res.send(`Users with the name ${user.name} added to the database!`);
 });
 
+// get a reques for a uique user with id ( with rq.params)
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  // we want to find our user
+  const findUser = users.find((user) => user.id === id);
+  res.send(findUser);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  // we use the filter methode to remove a user from our array
+  users = users.filter((user) => user.id !== id);
+  res.send(`Users with the id ${id} deleted from the database`);
+});
+
+//put is ude to update everything in the user
+// patch is used to update one thing
+router.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, lastName, age } = req.body;
+  const updateUser = users.find((user) => user.id == id);
+
+  if (name) {
+    updateUser.name = name;
+  }
+  if (lastName) {
+    updateUser.lastName = lastName;
+  }
+  if (age) {
+    updateUser.age = age;
+  }
+
+  res.send(`Users with the id ${id} dhas been updataed`);
+});
+
 module.exports = router;
+ 
